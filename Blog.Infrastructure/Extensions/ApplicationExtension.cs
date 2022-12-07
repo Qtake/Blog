af758883a00784a802d2852cb327fd13c784c107
Qtake <grant99458@gmail.com>
2022-12-08 02:17:08 +0300

    create application extension method

---
+++ Blog.Infrastructure/Extensions/ApplicationExtension.cs
@@ -0,0 +1,44 @@
+﻿using Blog.Domain.Entities;
+using Blog.Domain.Enums;
+using Blog.Infrastructure.Contexts;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blog.Infrastructure.Extensions
+{
+    public static class ApplicationExtension
+    {
+        public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
+        {
+            var context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<BlogContext>();
+            string[] roles = Enum.GetNames(typeof(RoleType));
+
+            foreach (var role in roles)
+            {
+                if (!context.Roles.Any(x => x.Name == role))
+                {
+                    context.Roles.Add(new Role(role));
+                }
+            }
+
+            context.SaveChanges();
+
+            var admin = new User
+            {
+                Name = "Admin1",
+                Email = "Admin@gmail.com",
+                Password = "123456",
+                Role = context.Roles.First(x => x.Name == RoleType.Admin.ToString())
+            };
+
+            if (!context.Users.Any())
+            {
+                context.Users.Add(admin);
+            }
+
+            context.SaveChanges();
+
+            return app;
+        }
+    }
+}
--- Blog.Infrastructure/Extensions/ServicesExtension.cs
+++ Blog.Infrastructure/Extensions/ServicesExtension.cs
@@ -1,9 +1,6 @@
-﻿using Blog.Domain.Entities;
-using Blog.Domain.Enums;
-using Blog.Infrastructure.Contexts;
+﻿using Blog.Infrastructure.Contexts;
 using Blog.Infrastructure.Repositories;
 using Blog.Service.Repositories;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,40 +29,5 @@ namespace Blog.Infrastructure.Extensions
 
             return services;
         }
-
-        public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
-        {
-            var context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<BlogContext>();
-            string[] roles = Enum.GetNames(typeof(RoleType));
-
-            foreach (var role in roles)
-            {
-                if (!context.Roles.Any(x => x.Name == role))
-                {
-                    context.Roles.Add(new Role(role));
-                }
-            }
-
-            context.SaveChanges();
-
-            //foreach (var user in context.Users)
-            //{
-            //    if (!context.Users.Any(x => x.Name == "Admin1"))
-            //    {
-            //        var admin = new User
-            //        {
-            //            Name = "Admin1",
-            //            Email = "Debik228@gmail.com",
-            //            Password = "123456",
-            //            Role = new Role { Name = RoleType.Admin.ToString() }
-            //        };
-            //    }
-            //}
-
-            //context.SaveChanges();
-
-            return app;
-        }
-
     }
 }
